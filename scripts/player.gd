59e1838de39150c00c686c19bcceccffb3c8ef11
Dave <dave.turmawan@outlook.com>
2025-02-14 20:42:29 +0700

    Fixed player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -250.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -9,7 +9,7 @@ func _physics_process(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		animated_sprite.play("jump")
